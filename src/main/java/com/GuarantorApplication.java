60013f965b5d35d8d87b74c3a03c7080347cf9a8
Olatunji Odelade <odeladetunji@gmail.com>
2019-10-25 17:38:34 +0100

    first commit

--- src/main/java/com/GuarantorApplication.java
+++ src/main/java/com/GuarantorApplication.java
@@ -1,4 +1,4 @@
-package com.guarantors.guarantor;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class GuarantorApplication {
     }
 
 }
+
