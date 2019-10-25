74682848b571332da9fa374a9d75f686c21a7591
Olatunji Odelade <odeladetunji@gmail.com>
2019-10-25 17:20:35 +0100

    first commit

---
+++ src/main/java/com/guarantors/guarantor/GuarantorApplication.java
@@ -0,0 +1,13 @@
+package com.guarantors.guarantor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarantorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuarantorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guarantors/guarantor/GuarantorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guarantors.guarantor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarantorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
